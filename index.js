9bfe08d08887918bd470b38eb6e84405dca31352
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-10-09 00:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching(array, name) {
+
+return array.filter((array1)=> { return array1 === name || array1.toUpperCase() === name.toUpperCase(); 
+
+}
+    )
+}
+
+
+function fuzzyMatch(drivers, para) {
+    let passedValue = para;
+    return drivers.filter((driver)=> {return driver.slice(0, 2) === passedValue
+    }
+    )  
+}
+
+
+function matchName(drivers, name) {
+let values
+values= (drivers.filter( (driver) => {return driver.name === name}))
+return values;
+}
